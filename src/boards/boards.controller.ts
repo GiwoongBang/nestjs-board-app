535e3f9bb004e7e939a14bc8fffd2928793acd6c
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-03-04 18:54:50 +0900

    feat: Create Board controllers

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './boards.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidatonPipe } from './pipes/board-status-validation.pipe';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoards(): Board[] { 
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoardById(@Param('id') id: string): void {
+    this.boardsService.deleteBoardById(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardById(
+    @Param('id') id: string,
+    @Body('status', BoardStatusValidatonPipe) status: BoardStatus,
+  ) {
+    return this.boardsService.updateBoardById(id, status);
+  }
+}
