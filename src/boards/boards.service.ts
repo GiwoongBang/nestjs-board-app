261b16e66f8afef776067d7e98a6e433652bd138
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-03-04 18:55:26 +0900

    feat: Create Board service

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Injectable()
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(),
+      title: title,
+      description: description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    const foundBoard = this.boards.find((board) => board.id === id);
+
+    if (!foundBoard) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    return foundBoard;
+  }
+
+  deleteBoardById(id: string): void {
+    const foundBoard = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== foundBoard.id);
+  }
+
+  updateBoardById(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
+    return board;
+  }
+} 
