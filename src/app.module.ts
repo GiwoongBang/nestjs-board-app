824ef6e5c97fd1b9bc6b16edf3505e14a2cce62b
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-03-06 18:57:15 +0900

    feat: Set the default settings for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeORMConfig), BoardsModule],
+  imports: [TypeOrmModule.forRoot(TypeORMConfig), BoardsModule, AuthModule],
 })
 export class AppModule {}
