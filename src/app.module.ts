74fe39a03b67c03841e1963814afdb2971e31501
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-03-12 16:59:45 +0900

    style: Alignment of code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { PaymentsModule } from './payments/payments.module';
+import { PaymentsController } from './payments/payments.controller';
+import { PaymentsService } from './payments/services/payments.service';
+import { OrderRepository } from './payments/repositories/payments-order.repository';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeORMConfig), BoardsModule, AuthModule, PaymentsModule],
+  imports: [
+    TypeOrmModule.forRoot(TypeORMConfig),
+    BoardsModule,
+    AuthModule,
+    PaymentsModule,
+  ],
+  controllers: [PaymentsController],
+  providers: [PaymentsService, OrderRepository],
 })
 export class AppModule {}
