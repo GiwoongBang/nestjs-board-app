7eab08f8eb287b78e1521a8a78e180016549b78a
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-03-14 01:13:39 +0900

    refactor: Add initializeTransactionalContext() for @Transactional

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,36 @@ import { AuthModule } from './auth/auth.module';
 import { PaymentsModule } from './payments/payments.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('RUNTIME') !== 'prod',
+        logging: configService.get<string>('RUNTIME') !== 'prod',
+      }),
+      async dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+        return addTransactionalDataSource(new DataSource(options));
+      },
+    }),
     TypeOrmModule.forRoot(TypeORMConfig),
     BoardsModule,
     AuthModule,
