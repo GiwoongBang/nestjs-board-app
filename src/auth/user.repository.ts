7c9c28c1c733d8b2e2baadfafae0ac05170dd2e1
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-03-14 01:17:46 +0900

    refactor: Modify repository constructor for TypeORM

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,20 @@
-import { DataSource, Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
-  constructor(dataSource: DataSource) {
-    super(User, dataSource.createEntityManager());
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectEntityManager()
+    private readonly entityManager: EntityManager,
+  ) {
+    super(
+      userRepository.target,
+      userRepository.manager,
+      userRepository.queryRunner,
+    );
   }
 }
